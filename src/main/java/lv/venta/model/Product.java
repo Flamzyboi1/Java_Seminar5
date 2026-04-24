335e20ffbcf2eb286a90efbbb14568f8a7b1d9a4
Favour <favourobidiaso@gmail.com>
2026-04-24 15:24:55 +0300

    Add: added update to show category as enumtype string

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -3,6 +3,8 @@ package lv.venta.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,6 +28,7 @@ public class Product {
 	private String title;
 	@Column(name = "Category")
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	
 	private Category category;
 	
